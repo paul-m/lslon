8f3bca205498479b18fbe30eca213e9856346ff6
Paul Mitchum <paul@mile23.com>
2013-04-07 20:31:36 -0700

    TYPED support and initial test

--- Src/PaulM/LSLON/ArrayToLSLONConverter.php
+++ Src/PaulM/LSLON/ArrayToLSLONConverter.php
@@ -5,38 +5,69 @@ class ArrayToLSLONConverter {
 
   protected $data;
   protected $lslon;
-  
+
   public function __construct(array $data) {
     $this->setData($data);
   }
-  
+
   public function setData(array $data) {
     $this->data = $data;
     $this->lslon = '';
   }
-  
+
   public function getLslon($typed = FALSE) {
     if (empty($this->lslon)) {
       $this->generateLslon($typed);
     }
     return $this->lslon;
   }
-  
+
   public function __toString() {
     return $this->getLslon();
   }
 
+/*
+TYPE_INTEGER	1	integer
+TYPE_FLOAT	2	float
+TYPE_STRING	3	string
+TYPE_KEY	4	key
+TYPE_VECTOR	5	vector
+TYPE_ROTATION	6	rotation
+TYPE_INVALID	0	none
+*/
+
   protected function generateLslon($typed = FALSE) {
     $lslonArray = ['LSLON 1.0'];
     if (!empty($this->data)) {
+      $valueArray = array();
       // data should be an array due to type hinting.
       foreach($this->data as $name=>$value) {
-        
+        if ($typed) $valueArray[] = 'TYPED';
+        switch (gettype($value)) {
+          case 'boolean' :
+            // http://wiki.secondlife.com/wiki/TRUE
+            $value = $value ? 1 : 0;
+            // Fall through to integer...
+          case 'integer' :
+            if ($typed) $valueArray[] = '1';
+            $valueArray[] = $value;
+            break;
+          case 'double' :
+            if ($typed) $valueArray[] = '2';
+            $valueArray[] = $value;
+            break;
+          case 'string' :
+            if ($typed) $valueArray[] = '3';
+            $valueArray[] = urlencode($value);
+            break;
 
-        $lslonArray[] = urlencode($name) . '=' . $value;
+          default:
+            throw new \InvalidArgumentException('LSLON can only encode bool, int, float, string types.');
+        }
+        $lslonArray[] = urlencode($name) . '=' . implode('|', $valueArray);
       }
     }
-    
+
     $this->lslon = implode("\n", $lslonArray);
   }
 
--- Test/PaulM/LSLON/ArrayToLSLONConverterTest.php
+++ Test/PaulM/LSLON/ArrayToLSLONConverterTest.php
@@ -3,14 +3,35 @@ namespace PaulM\LSLON;
 
 use PaulM\LSLON\ArrayToLSLONConverter;
 
+/**
+ * @todo: test for good complex conversions.
+ */
+
 class ArrayToLSLONConverterTest extends \PHPUnit_Framework_TestCase {
-  
-  public function testArrayConversion() {
+
+  public function testConversion() {
     $converter = new ArrayToLSLONConverter(array());
     $this->assertEquals($converter->getLslon(), 'LSLON 1.0');
     $converter->setData(array('foo' => 'bar'));
     $this->assertEquals($converter->getLslon(), "LSLON 1.0\nfoo=bar");
   }
   
+  public function badInputProvider() {
+    return array(
+      array(array(array())),
+      array(array(new \stdClass())),
+      array(array(NULL)),
+    );
+  }
+  
+  /**
+   * @dataProvider badInputProvider
+   * @expectedException \InvalidArgumentException
+   */
+  public function testBadInput(array $array) {
+    $converter = new ArrayToLSLONConverter($array);
+    $lslon = $converter->getLslon();
+  }
+
 }
 
